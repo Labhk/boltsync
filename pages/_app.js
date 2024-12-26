4d4a1cb7f67d19d7225656af311e08d671802add
Labh <79000376+Labhk@users.noreply.github.com>
2024-12-26 15:41:10 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,25 @@
 import { SessionProvider } from "next-auth/react";
 import Head from "next/head";
 import "../styles/globals.css";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
-function MyApp({ Component, pageProps }) {
+// Initialize PostHog client-side
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
+    },
+  });
+}
+
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <>
       <Head>
+        {/* Microsoft Clarity */}
         <script
           type="text/javascript"
           dangerouslySetInnerHTML={{
@@ -18,12 +32,34 @@ function MyApp({ Component, pageProps }) {
             `,
           }}
         />
+
+        {/* Matomo */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var _paq = window._paq = window._paq || [];
+              /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u="https://mystify.matomo.cloud/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '1']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.async=true; g.src='https://cdn.matomo.cloud/mystify.matomo.cloud/matomo.js'; 
+                s.parentNode.insertBefore(g,s);
+              })();
+            `,
+          }}
+        />
       </Head>
-      <SessionProvider session={pageProps.session}>
-        <Component {...pageProps} />
-      </SessionProvider>
+
+      {/* PostHog Provider */}
+      <PostHogProvider client={posthog}>
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </PostHogProvider>
     </>
   );
 }
-
-export default MyApp;
